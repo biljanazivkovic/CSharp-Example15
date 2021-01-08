9deb6b71aa4a6d775791139e443beb996bc6450b
Biljana Zivkovic <77129646+biljanazivkovic@users.noreply.github.com>
2021-01-08 16:31:13 +0100

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex15
+{
+    class Person
+    {
+        /*
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        */
+        public string Name
+        { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person myObj = new Person();
+            myObj.Name = "Liam";
+            Console.WriteLine(myObj.Name);
+        }
+    }
+}
